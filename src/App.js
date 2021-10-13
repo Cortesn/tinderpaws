47b58c72607451d581f14bcf779a345ffe5a8b3c
Anthony-ferrari <anthonyferrari1611@gmail.com>
2021-10-12 21:34:41 -0700

    created MissionPage and added react router and switch for routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from "react";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import HomePage from "./screens/HomePage";
+import MissionPage from "./screens/MissionPage";
+
 
 function App() {
-  return <HomePage />;
+  return (
+    <Router>
+    <div className="App">
+      {/* <Navbar /> */}
+      <div className="content">
+        {/* can be a string, int, float, array, even math.random */}
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/mission">
+            <MissionPage />
+          </Route>
+        </Switch>
+      </div>
+    </div>
+  </Router>
+  )
 }
 
 export default App;
---
+++ src/screens/MissionPage.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { Typography, Box, Button, Stack } from "@mui/material";
+import createIntro from "../helperFunctions/HomePage/createIntroduction";
+
+const MissionPage = () => {
+    const text = "Our mission here at Tinder Paws is to find loving homes for members of the various shelters we partner with.\n We strive to make the matching process easy for you and want our users to focus on finding a mutual connection.\n We want to increase the visibility of highly adoptable pets and increase the overall effectiveness of pet adoption programs and applications."
+    return ( 
+        <Box>
+            {/* Navbar goes here */}
+            <Box sx={{
+                border:1,
+                borderColor: 'grey.500',
+                width: '60%',
+                margin: 'auto',
+                padding: '1rem',
+                borderRadius:"12px",
+                marginBottom: "2rem"
+            }}>
+                <Typography 
+                variant="h4"
+                align="center"
+                gutterBottom>
+                    Our Mission
+                </Typography>
+
+                <Typography
+                component="span"
+                ariant="body1"
+                align="center">
+                    {createIntro(text)}
+                </Typography>
+            </Box>
+            <Stack
+            alignItems="center"
+            justifyContent="space-evenly">
+                <Button variant="contained" color="success" href="/signUp">
+                    Start your journey
+                </Button>
+            </Stack>
+        </Box>
+     );
+}
+ 
+export default MissionPage;
