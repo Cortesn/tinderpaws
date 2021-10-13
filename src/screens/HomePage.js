29b89ab70259dcd0ea213b829979ae7a27f5d851
Anthony-ferrari <anthonyferrari1611@gmail.com>
2021-10-12 20:18:29 -0700

    added down arrow icon and changed stack logic

--- src/screens/HomePage.js
+++ src/screens/HomePage.js
@@ -4,7 +4,7 @@ import createIntro from "../helperFunctions/HomePage/createIntroduction";
 import createFlowchartItems from "../helperFunctions/HomePage/createFlowchartItems"
 
 const HomePage = () => {
-    const text = "Welcome to Tinder Paws!\n We are as excited as you to being your companion finding journey!\n Here you will able to look through all animals at different shelters and match with the ones you like.\n Our team will then contact you to start an application process and for you to meet your match.\n After the screening, you will be able to take your companion home!\n Here are the steps:"
+    const text = "Welcome to Tinder Paws!\n We are as excited as you to being your companion finding journey!\n Here you will able to look through all animals at different shelters and match with the ones you like.\n Our team will then contact you to start an application process and for you to meet your match.\n After the screening, you will be able to take your companion home!"
     const sequence = "Create an account\n Look through available animals\n Match with one of the animals\n Start application and visit your match\n Adopt and Pick Up\nEnjoy life with your companion"
     return ( 
         <Box>
@@ -15,7 +15,8 @@ const HomePage = () => {
                 width: '60%',
                 margin: 'auto',
                 padding: '1rem',
-                borderRadius:"12px"
+                borderRadius:"12px",
+                marginBottom: "2rem"
             }}>
                 <Typography 
                 variant="h4"
@@ -35,7 +36,7 @@ const HomePage = () => {
             alignItems="center"
             justifyContent="space-evenly">
                 {createFlowchartItems(sequence)}
-                <Button variant="contained" color="success" href="/signUp" sx={{ marginTop:"2rem"}}>
+                <Button variant="contained" color="success" href="/signUp">
                     Get Started!
                 </Button>
             </Stack>
