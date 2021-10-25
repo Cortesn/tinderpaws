a6d8d6df85e1c2297feeb9eeb309584569384595
Cortesn <59752457+Cortesn@users.noreply.github.com>
2021-10-24 23:53:25 -0700

    Added functionality to click through pictures in Adminpage

--- client/src/Components/adminpage/PetCard.js
+++ client/src/Components/adminpage/PetCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {
 	Button,
 	Card,
@@ -7,9 +7,25 @@ import {
 	CardMedia,
 	Grid,
 	Typography,
+	IconButton,
 } from "@mui/material";
+import { NavigateBefore, NavigateNext } from "@mui/icons-material";
 
 export const PetCard = ({ pet, deletePet }) => {
+  const [imgIdx, setImgIdx] = useState(0);
+
+	const prevImg = () => {
+		if (imgIdx > 0) {
+			setImgIdx(imgIdx - 1);
+		}
+	};
+
+	const nextImg = () => {
+		if (imgIdx < pet.images.length - 1) {
+			setImgIdx(imgIdx + 1);
+		}
+	};
+
 	return (
 		<Grid
 			item
@@ -21,13 +37,36 @@ export const PetCard = ({ pet, deletePet }) => {
 			sx={{ px: 0 }}
 		>
 			<Card sx={{ width: 345 }}>
-				<CardMedia
-					component="img"
-					alt={pet.name}
-					image={pet.images[0]}
-					height="300"
-					sx={{ objectFit: "cover" }}
-				/>
+				<Grid container sx={{position:"relative"}}>
+					<IconButton onClick={prevImg}
+						sx={{
+							position: "absolute",
+							color: "white",
+							top: "40%",
+              display: imgIdx===0 ? "none" : "block"
+						}}
+            >
+						<NavigateBefore fontSize="large" />
+					</IconButton>
+					<CardMedia
+						component="img"
+						alt={pet.name}
+						image={pet.images[imgIdx]}
+						height="300"
+						sx={{ objectFit: "cover" }}
+            />
+					<IconButton onClick={nextImg}
+						sx={{
+							position: "absolute",
+							color: "white",
+							top: "40%",
+              right: "0",
+              display: imgIdx===pet.images.length - 1 ? "none" : "block"
+						}}
+					>
+						<NavigateNext fontSize="large" />
+					</IconButton>
+				</Grid>
 				<CardContent sx={{ textAlign: "center" }}>
 					<Typography variant="h5">{pet.name}</Typography>
 				</CardContent>
