868b51847b3ecdc7f4d480992f547da3f8a028e2
Cortesn <59752457+Cortesn@users.noreply.github.com>
2021-10-25 22:34:24 -0700

    Installed dotenv package to use with dbcon

--- server/Database/dbcon.js
+++ server/Database/dbcon.js
@@ -1,13 +1,15 @@
 /* 
 Connects to a MariaDB instance running on Amazon RDS
 */
+import dotenv from 'dotenv';
+dotenv.config();
 
 import mysql from 'mysql';
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : [endpointAddress],
-  user            : [userName],
-  password        : [password],
-  database        : [databaseName]
+  host            : process.env.SERVER_ADDRESS,
+  user            : process.env.USER,
+  password        : process.env.PASSWORD,
+  database        : process.env.DATABASE
 });
 export default pool;
