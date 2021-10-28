94fdc30a0cdc33fc230f3634de10db3812e5c1f2
Cortesn <59752457+Cortesn@users.noreply.github.com>
2021-10-27 20:58:37 -0700

    small changes to routes

--- server/Database/dbcon.js
+++ server/Database/dbcon.js
@@ -5,9 +5,9 @@ Connects to a MariaDB instance running on Amazon RDS
 import mysql from 'mysql';
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : [endpointAddress],
-  user            : [userName],
-  password        : [password],
-  database        : [databaseName]
+  host            : process.env.DB_HOST,
+  user            : process.env.DB_USER,
+  password        : process.env.DB_PASSWORD,
+  database        : process.env.DB_NAME
 });
 export default pool;
