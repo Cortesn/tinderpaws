f46853b76e5844e0193e1f95bd8e2bc780d2e63b
Cortesn <59752457+Cortesn@users.noreply.github.com>
2021-10-30 00:52:29 -0700

    Integrated user home, debugging swiping

--- server/routes/userpage.js
+++ server/routes/userpage.js
@@ -5,7 +5,11 @@ import db from '../Database/dbcon.js'
 
 // return all available shelters in the db
 userRouter.get('/pets', (req, res) => {
-    const getPets = 'SELECT pet_id, name, breed, description FROM Pets;'
+    const getPets = `SELECT p.*, a.type as animalType, GROUP_CONCAT(i.url) as images 
+    FROM Pets as p
+    INNER JOIN Images as i ON p.pet_id=i.pet_id
+    INNER JOIN Animals as a ON p.type=a.animal_id
+    GROUP BY p.pet_id;`
     db.query(getPets, (error, results) => {
         if (error){
             console.log(error)
